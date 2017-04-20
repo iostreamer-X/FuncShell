9e3092b0a5a4d1e6a071ee4584d6e307c785f8ed
iostreamer-X <iostreamer007@gmail.com>
2017-04-20 19:09:53 +0530

    Added comments, fixed bug when reading from pipe
    I was using 'getLine' to read from pipe which resulted premature ending of input. Now using 'getContents' to handle this.

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,20 @@ module Main where
 import Language.Haskell.Interpreter as I
 import System.Environment
 
-replace :: Char->Char->String->String
-replace fromC toC = map (\c -> if c==fromC then toC else c)
+-- The output of the bash command is converted to a list of strings
+-- and this list needs to represented as a String to be interpreted.
+-- To do this, each string in the list is enclosed with quotes
+-- and then joined together to get string representation of list.
 
-encloseWithQuotes = map (\s->"\""++s++"\"")
-listToString str = "[" ++ (init $ foldr (\s1 s2->s1++","++s2) "" str) ++ "]"
-
-cleanFunction :: String->String
-cleanFunction str = init.tail $ str
+-- The way this is achieved is each string in list is mapped to {,"str"},
+-- so ["1","2"] becomes [ ",\"1\"" , ",\"2\"" ]
+-- Now all that is left is join the list, hence unwords, but we get a leading {,}
+-- hence `tail` 
+encloseWithQuotes = map (\s->",\""++s++"\"")
+listToString str = "[" ++ (tail.unwords $ str) ++ "]"
 
 main = do functionPieces <- getArgs
-          args <- getLine
+          args <- getContents
           functionStr <- return (unwords functionPieces)
           processedArgs <- return $ listToString.encloseWithQuotes $ words args
           result <- runInterpreter $ setImports ["Prelude"] >> interpret (functionStr ++ " " ++ processedArgs) (as :: [String])
